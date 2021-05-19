b19578917bd850ccea41de622888304adf9972fe
Daniel Lewis <daniel.lewis@protonmail.com>
2021-05-19 14:49:46 -0600

    added CRUD operations

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -36,6 +36,30 @@ const pizzaController = {
   },
 
   // update pizza by ID
+  updatePizza({ params, body }, res) {
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "there is no pizza here" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // delete pizza
+  deletePizza({ params }, res) {
+    Pizza.findOneAndDelete({ _id: params.id })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "there is no pizza here" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
 };
 
 module.exports = pizzaController;
