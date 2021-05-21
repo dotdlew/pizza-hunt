794cc7febe603e691f3404db3ed57c7f7b15d6cf
Daniel Lewis <daniel.lewis@protonmail.com>
2021-05-20 18:54:30 -0600

    updated add-replies feature

--- models/Comment.js
+++ models/Comment.js
@@ -37,7 +37,7 @@ const CommentSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal),
+      // get: (createdAtVal) => dateFormat(createdAtVal),
     },
     replies: [ReplySchema],
   },
