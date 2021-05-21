9aaa7629fa22cf90b41458d7e596fab585ab0a88
Daniel Lewis <daniel.lewis@protonmail.com>
2021-05-20 18:41:34 -0600

    added replies

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,12 +2,17 @@ const router = require("express").Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require("../../controllers/comment-controller");
 
-// /api/comments/<pizzaId>
+// /api/comments/<pizzaId> - add comment
 router.route("/:pizzaId").post(addComment);
 
-// /api/comments/<pizzaId>/<commentId>
-router.route("/:pizzaId/:commentId").delete(removeComment);
+// /api/comments/<pizzaId>/<commentId> - add reply or delete comment
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
+
+// /api/comments/<pizzaId>/<commentId>/<replyId> - delete reply
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 
 module.exports = router;
